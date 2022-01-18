de02286c0fd25fa61ab83f136d9854d077f1549c
CarlosVelilla <carlosvelillaf@hotmail.com>
2022-01-18 18:27:21 +0100

    Start styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import styles from "./Header.module.css"
+import logo from "../../assets/img/logo.svg"
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className={styles.container}>
+      <div className={styles.header__logoContainer}>
+        <img src={logo} alt="Company logo" className={styles.header__logoContainer_img} />
+        <div>
+          <div className={styles.header__logoContainer_title}>Mangohome</div>
+          <div className={styles.header__logoContainer_subtitle}>Where rice meets mangoes</div>
+        </div>
+        
+      </div>
+      <nav>
+        <ul className={styles.header__menu}>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Macu</li>
+          <li>Macu</li>
+          <button className={styles.secondary_btn}>Sign in</button>
+          <button className={styles.primary_btn}>Sign up</button>
+        </ul>
+      </nav>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import SearchInput from "../../components/SearchInput/SearchInput"
+import styles from "./Home.module.css"
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <SearchInput isHome={true} />
+      <main>
+        <h1>Home</h1>
+        <SearchInput isHome={true} />
+      </main>
+      <section>
+        <span>Popular listings</span>
+        <article>
+          <span className={styles.listings__title}>Buy</span>
+        </article>
+        <article>
+          <span className={styles.listings__title}>Rent</span>
+
+        </article>
+      </section>
     </>
   )
 }
